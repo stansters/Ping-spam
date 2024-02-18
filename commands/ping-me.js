232be7762895e332b948261dc8908eecbcf3f93d
stansters <stanleygeorgebond@gmail.com>
2024-02-18 13:21:48 +0000

    message count check + more console info
    I found that sending more than 500000 messages in one command made it crash
    
    Added messages per second to console log cos i was intrigued if it was true
    
    Changed timer starting time from 1 to 0 as i noticed that the first console log always started on 2 seconds, making info inaccurate

--- commands/ping-me.js
+++ commands/ping-me.js
@@ -23,8 +23,7 @@ module.exports = {
           console.log('(2) Times set to ' + times)
         }
         const db = new sqlite3.Database('database.db');
-        await interaction.reply('Working on your request!');
-
+        await interaction.reply(`Working on your request!`)
 
         db.all('SELECT id FROM channels', [], (err, rows) => {
             if (err) {
@@ -33,15 +32,23 @@ module.exports = {
             }
         const totalRows = (rows.length*times)
         console.log('Amount of messages to send: '+totalRows)
-        
+
+        if (totalRows > 500000){
+          interaction.editReply(`Sorry! ${totalRows} messages is above my 500000 message limit! Please try again with a lower per-channel message count, too many and i will crash!`)
+          console.log('Too many messages were requested')
+          console.log('Cancelling...')
+        } else {
+        interaction.editReply(`Working on your request for ${totalRows} messages!`)
+
         function timerfunc(){
           timer = (timer+1)
         }
-        var timer = 1
+        var timer = 0
         var runs = 0
         var percentdone = 0
         var minsleft = 0
         var timeleft = 0
+        var messagepers = 0
         const timerVar = setInterval(timerfunc, 1000)
         fs.writeFileSync('running',' ');
             for (let i = 0; i < times; i++){
@@ -53,8 +60,9 @@ module.exports = {
                     .then(() => runs = (runs+1))
                     .then(() => percentdone = ((runs/totalRows)*100).toFixed(3))
                     .then(() => minsleft = Math.abs((((timer/runs)*(runs-totalRows))/60).toFixed(2)))
+                    .then(() => messagepers = Math.abs((runs/timer).toFixed(1)))
                     .then(() => {if (minsleft < 1){timeleft = Math.round((minsleft*60))+' seconds'} else {timeleft = minsleft+' minutes'}})
-                    .then(() => console.log(`Sent message #${runs} in channel ${channel.name} with ID ${channel.id} (${percentdone}% done & ${timeleft} left) ${timer}s`))
+                    .then(() => console.log(`Sent message #${runs} in channel ${channel.name} with ID ${channel.id} (${percentdone}% done & ${timeleft} left) ${timer}s ${messagepers}m/s`))
                     .then(() => {if (runs == totalRows){
                       clearInterval(timerVar)
                       fs.unlinkSync('running')
@@ -66,7 +74,7 @@ module.exports = {
                 }
               }
             }
-        });  
+        }});  
       }},
     };
 	
